256080feff9b683a13455c6c6c2f2430ef143224
Nadav Spitzer <nspitzern@gmail.com>
2023-04-13 18:50:52 +0300

    Create separate function for getting kl divergence of bands

---
+++ common_utils/information_theory.py
@@ -0,0 +1,30 @@
+from typing import Union
+
+import numpy as np
+from numpy import ndarray
+
+
+def kl(
+        p: np.ndarray,
+        q: np.ndarray
+) -> Union[int, ndarray]:
+    """Kullback-Leibler divergence D(P || Q) for discrete distributions
+    Parameters
+    ----------
+    p, q : array-like, dtype=float, shape=n
+    Discrete probability distributions.
+    """
+    p = np.array(p, dtype=np.float)
+    q = np.array(q, dtype=np.float)
+
+    if p == q:
+        return 0
+
+    return np.sum(np.where(q != 0, p * np.log(p / q), 0))
+
+
+def dkl(
+        p: np.ndarray,
+        q: np.ndarray
+) -> Union[int, ndarray]:
+    return kl(p, q) + kl(q, p)
