Fix KL condition for both p !=0 and q != 0. Fix KL condition for p == q

common_utils/information_theory.py
@@ -17,10 +17,10 @@ def kl(
     p = np.array(p, dtype=np.float)
     q = np.array(q, dtype=np.float)
 
-    if p == q:
+    if np.all(p == q):
         return 0
 
-    return np.sum(np.where(q != 0, p * np.log(p / q), 0))
+    return np.sum(np.where((q != 0) & (p != 0), p * np.log(p / q), 0))
 
 
 def dkl(