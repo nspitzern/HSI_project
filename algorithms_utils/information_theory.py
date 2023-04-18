Fix KL divergence to handle NaN from 0 in P

algorithms_utils/information_theory.py
@@ -22,7 +22,7 @@ def kl(
     if np.all(p == q):
         return 0
 
-    return np.sum(np.where(q != 0, p * np.log(p / q), 0))
+    return np.sum(np.where((q != 0) & (p != 0), p * np.log(p / q), 0))
 
 
 def dkl(