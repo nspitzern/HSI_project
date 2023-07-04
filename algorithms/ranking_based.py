9fca7cd8e39a8bdc48fce6ab8c3b4ebcc7b51166
Nadav Spitzer <nspitzern@gmail.com>
2023-07-04 14:24:29 +0300

    Add divergence-ranking based band selection algorithm

--- algorithms/ranking_based.py
+++ algorithms/ranking_based.py
@@ -41,14 +41,15 @@ class MMCA(BaseAlgorithm):
 
         idxs = self._rank_bands(X, clusters)
 
-        bands = _divergence_based_band_selection(X, idxs, eps=eps)
+        # bands = _divergence_based_band_selection(X, idxs.tolist(), eps=eps)
+        bands = _ranking_based_selection(X, idxs.tolist(), self.n_classes)
         return X[:, bands], bands
 
     def _rank_bands(
             self,
             X: np.ndarray,
             clusters: np.ndarray = None
-    ) -> List[int]:
+    ) -> np.ndarray:
         """
         Implementation is similar to LDA here: https://www.youtube.com/watch?v=9IDXYHhAfGA&t=762s
         :param X: Bands vectors (size: (H*W,N_BANDS))
@@ -58,8 +59,11 @@ class MMCA(BaseAlgorithm):
         """
         num_features, num_bands = X.shape
 
-        if clusters is not None:
+        if clusters is None:
             clusters = KMeans(self.n_classes, n_init='auto').fit_predict(X)
+
+        if len(clusters.shape) > 1:
+            clusters = clusters.flatten()
 
         m = np.mean(X, axis=0)
         S_W = np.zeros([num_bands, num_bands])
@@ -132,14 +136,59 @@ def _divergence_based_band_selection(
     return np.array(final_group)
 
 
+def _ranking_based_selection(
+    X: np.ndarray,
+    bands_priorities: List[int],
+    n_classes: int,
+    top_percentage: float = 0.1
+) -> np.ndarray:
+    top_k_to_pick = int(top_percentage * 10)
+    highest_priority_band = bands_priorities[0]
+    final_group = [highest_priority_band]
+
+    bands_ranks = {band: i for i, band in enumerate(bands_priorities)}
+
+    num_bands = X.shape[-1]
+
+    bands_histograms = [get_band_histogram(X[:, i], density=True) for i in range(num_bands)]
+
+    candidate_bands = list(set(bands_priorities) - set(final_group))
+    while len(final_group) < n_classes:
+        candidates_distances = dict()
+        # Calculate DKL between each band in final group and all the rest
+        for c_band in candidate_bands:
+            candidates_distances[c_band] = [dkl(bands_histograms[c_band], bands_histograms[selected_band])
+                                            for selected_band in final_group]
+
+        # For each band not in final group keep only the min distance to other bands in final group
+        for c_band in candidate_bands:
+            candidates_distances[c_band] = min(candidates_distances[c_band])
+
+        # Sort bands by minimal distance and take max 10%
+        bands_order_by_distance = {k: v for k, v in sorted(candidates_distances.items(), key=lambda item: item[1], reverse=True)}
+        top_bands = list(bands_order_by_distance.keys())[:max(1, len(bands_order_by_distance.keys())//top_k_to_pick)]
+
+        # Get the rank of each band in 10%
+        top_bands_ranks = {band: bands_ranks[band] for band in top_bands}
+        top_bands_ranks = {k: v for k, v in sorted(top_bands_ranks.items(), key=lambda item: item[1])}
+
+        # Get the max rank band and add to final group
+        top_band = list(top_bands_ranks.keys())[0]
+
+        final_group.append(top_band)
+        candidate_bands.remove(top_band)
+
+    return np.array(final_group)
+
+
 @timeit(num_repeats=1)
 def main():
     a = np.random.randint(0, 255, (700, 670, 210))
     w = MMCA(n_classes=10)
     w.fit(a)
-    pred = w.predict(a)
-    print(len(pred))
-    print(pred)
+    bands, idxs = w.predict(a)
+    print(len(idxs))
+    print(idxs)
 
 
 if __name__ == '__main__':
