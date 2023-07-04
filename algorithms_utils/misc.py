3c0634f26289a52165aba5d92ef761d5c4f8cb01
Nadav Spitzer <nspitzern@gmail.com>
2023-07-04 14:17:21 +0300

    Add cache class

--- algorithms_utils/misc.py
+++ algorithms_utils/misc.py
@@ -1,3 +1,6 @@
+from typing import Dict
+from random import choice
+
 import numpy as np
 
 
@@ -6,3 +9,22 @@ def get_band_histogram(
         density: bool = False
 ) -> np.ndarray:
     return np.histogram(band, bins=256, range=(0, 256), density=density)[0]
+
+
+class Cache:
+    def __init__(self, max_size):
+        self.__max_size = max_size
+        self.__buffer: Dict = dict()
+
+    def __setitem__(self, key, val):
+        if len(self.__buffer.keys()) >= self.__max_size:
+            k = choice(list(self.__buffer.keys()))
+            del self.__buffer[k]
+
+        self.__buffer[key] = val
+
+    def __getitem__(self, item) -> float:
+        return self.__buffer[item]
+
+    def get(self, item) -> float:
+        return self.__buffer.get(item)
